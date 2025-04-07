d424dba75faf6c195429ee79d5a9f6adfb371340
Estiben17 <estibendavila1705@gmail.com>
2025-04-07 12:25:18 -0500

    mo

--- backend/db.py
+++ backend/db.py
@@ -1,14 +1,28 @@
 import psycopg2
 
-conexion = psycopg2.connect(
-    host="localhost",
-    port="5432",
-    database="inventario",
-    user="postgres",
-    password="Luna12"
-)
+def get_connection():
+    return psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="inventario",     # Asegúrate de que esta base exista
+        user="postgres",           # Tu usuario de PostgreSQL
+        password="tu_contraseña"   # Cámbialo por tu contraseña real
+    )
 
-print("Conexión exitosa a PostgreSQL")
+def init_db():
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        with open("./BD/schema.sql", "r") as archivo:
+            sql = archivo.read()
+            cursor.execute(sql)
+            conn.commit()
+        print("✅ Tablas creadas correctamente.")
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print("❌ Error al crear las tablas:", e)
 
-# No olvides cerrar después:
-conexion.close()
+# Ejecutar si se corre directamente
+if __name__ == "__main__":
+    init_db()
